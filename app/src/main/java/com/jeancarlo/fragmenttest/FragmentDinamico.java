78e9c69ea022f4272d1d1181a914599a0869e325
Jean Carlo <sebastiancordova473@gmail.com>
2018-02-04 16:47:12 -0500

    fin de fragment statico

--- app/src/main/java/com/jeancarlo/fragmenttest/FragmentDinamico.java
+++ app/src/main/java/com/jeancarlo/fragmenttest/FragmentDinamico.java
@@ -1,13 +1,57 @@
 package com.jeancarlo.fragmenttest;
 
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class FragmentDinamico extends AppCompatActivity {
+public class FragmentDinamico extends AppCompatActivity implements fragAzul.OnFragmentInteractionListener,fragRojo.OnFragmentInteractionListener,FragVerde.OnFragmentInteractionListener{
+
+    //Referencias a los fragments
+    FragVerde fVerde;
+    fragAzul fAzul;
+    fragRojo fRojo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_dinamico);
+
+        //Inicio
+        fVerde=new FragVerde();
+        fAzul=new fragAzul();
+        fRojo=new fragRojo();
+        //Castear Botones
+        getSupportFragmentManager().beginTransaction().add(R.id.contendor,fRojo).commit();
+
+
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    public void onClick(View view){
+        FragmentTransaction trans=getSupportFragmentManager().beginTransaction();
+        //ver que boton es
+        switch (view.getId()){
+            case R.id.btnRojo:
+                //Remplazar
+                trans.replace(R.id.contendor,fRojo);
+                break;
+            case R.id.btnAzul:
+                //Remplazar
+                trans.replace(R.id.contendor,fAzul);
+                break;
+            case R.id.btnVerde:
+                //Remplazar
+                trans.replace(R.id.contendor,fVerde);
+                break;
+        }
+        trans.commit();
     }
 }
--- app/src/main/java/com/jeancarlo/fragmenttest/MainActivity.java
+++ app/src/main/java/com/jeancarlo/fragmenttest/MainActivity.java
@@ -1,15 +1,31 @@
 package com.jeancarlo.fragmenttest;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements fragUno.OnFragmentInteractionListener,fragDos.OnFragmentInteractionListener{
+    //Declara Boton
+    Button btnFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Castear boton
+        btnFrag= (Button) findViewById(R.id.btnCambio);
+        //Accion del boton
+        btnFrag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Accion
+                Intent a=new Intent(getApplicationContext(),FragmentDinamico.class);
+                startActivity(a);
+            }
+        });
     }
 
     @Override
